478691f9cad1f8241181ba633d62376c86ba06e3
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-13 21:38:24 +0800

    finishing

--- tugas1-D121221002.py
+++ tugas1-D121221002.py
@@ -1,5 +1,6 @@
 # from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QLabel, QWidget
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QLabel, QWidget, QDesktopWidget
+from PyQt5 import QtCore 
 
 class MyWindow(QMainWindow):
   def __init__(self):
@@ -14,7 +15,17 @@ class MyWindow(QMainWindow):
     
     self.setGeometry(0, 0, 500, 500) # set posisi (x, y, width, height) dari MyWindow
     self.setWindowTitle("Belajar PyQt5") # set judul dari windows yang terbuka untuk MyWindow
-
+    
+    # memindahkan ke tengah windows
+    cwa = self.frameGeometry()
+    cwc = QDesktopWidget().availableGeometry().center()
+    cwa.moveCenter(cwc)
+    self.move(cwa.topLeft())
+    self.setFixedSize(500,500)
+    # menghilangkan salah satu tombol di kanan atas
+    # self.setWindowFlag(QtCore.Qt.FramelessWindowHint) 
+    # self.setWindowFlag(QtCore.Qt.WindowMinimizedButtonHint,False)
+    # self.setWindowFlag(QtCore.Qt.WindowCloseButtonHint,False) 
 
 app = QApplication([])
 window = MyWindow()
