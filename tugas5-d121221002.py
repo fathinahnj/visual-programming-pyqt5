d4ba3ca4bbec3f1f2dba0c0534e6954a69715f05
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-14 00:34:31 +0800

    add form

--- tugas5-d121221002.py
+++ tugas5-d121221002.py
@@ -1,11 +1,13 @@
 # from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtWidgets import QFormLayout, QApplication, QPushButton, QMainWindow, QLabel, QWidget, QDesktopWidget
+from PyQt5.QtWidgets import QFormLayout, QLineEdit, QApplication, QPushButton, QMainWindow, QLabel, QWidget, QDesktopWidget
 from PyQt5.QtCore import Qt
 
 class MyWindow(QMainWindow):
   def __init__(self):
     super().__init__() 
     layout = QFormLayout()
+    layout.addRow("Nama", QLineEdit())
+    layout.addRow("Email", QLineEdit())
     
     # kalau pakai MyWindow(QMainWindow), maka harus menggunakan set central widget
     widget = QWidget()
