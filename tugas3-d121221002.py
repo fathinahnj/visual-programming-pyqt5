f02248080b7602519de65fc7203c6d52aa93e74e
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-13 22:01:28 +0800

    add QHBoxLayout

---
+++ tugas3-d121221002.py
@@ -0,0 +1,27 @@
+# from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import QHBoxLayout, QApplication, QPushButton, QMainWindow, QLabel, QWidget, QDesktopWidget
+
+class MyWindow(QWidget):
+  def __init__(self):
+    super().__init__() 
+    layout = QHBoxLayout()
+    btn1 = QPushButton("button 1")
+    btn2 = QPushButton("button 2")
+    btn3 = QPushButton("button 3")
+    btn4 = QPushButton("button 4")
+    layout.addWidget(btn1)
+    layout.addWidget(btn2)
+    layout.addWidget(btn3)
+    layout.addWidget(btn4)
+    self.setLayout(layout) # akses QWidget (self), setLayout sesuai dengan "layout"
+    
+    # kalau pakai MyWindow(QMainWindow), maka harus menggunakan set central widget
+    # widget = QWidget()
+    # widget.setLayout(layout)
+    # self.setCentralWidget(widget)
+    
+    
+app = QApplication([])
+window = MyWindow()
+window.show()
+app.exec() 
