1c93db4934ac069bd9644f60e30183b4095ceb19
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-12-13 21:41:11 +0800

    add buttons

--- tugas2-d121221002.py
+++ tugas2-d121221002.py
@@ -4,6 +4,11 @@ from PyQt5.QtWidgets import QVBoxLayout, QApplication, QPushButton, QMainWindow,
 class MyWindow(QMainWindow):
   def __init__(self):
     super().__init__() 
+    layout = QVBoxLayout()
+    btn1 = QPushButton("button 1")
+    btn2 = QPushButton("button 2")
+    btn3 = QPushButton("button 3")
+    btn4 = QPushButton("button 4")
     
     
 app = QApplication([])
